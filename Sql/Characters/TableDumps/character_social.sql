b8e777d9470fc73cfe11bdf1b27f6f3c46fc77d4
WoW Adict <wow_adict@hotmail.com>
2013-11-11 11:33:25 -0600

    Updated character_social table to Mangos Zero.

--- Sql/Characters/TableDumps/character_social.sql
+++ Sql/Characters/TableDumps/character_social.sql
@@ -1,34 +1,35 @@
-/*
-SQLyog Community v8.5 
-MySQL - 5.1.45-community : Database - spuriouszero
-*********************************************************************
+/*
+SQLyog Ultimate - MySQL GUI v8.2 
+MySQL - 5.5.33 : Database - mangosvbcharacters
+*********************************************************************
 */
-
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`spuriouszero` /*!40100 DEFAULT CHARACTER SET latin1 */;
-
-/*Table structure for table `characters_social` */
-
-DROP TABLE IF EXISTS `characters_social`;
-
-CREATE TABLE `characters_social` (
-  `char_guid` bigint(20) unsigned NOT NULL,
-  `guid` bigint(20) unsigned NOT NULL,
-  `note` varchar(255) NOT NULL,
-  `flags` smallint(6) NOT NULL,
-  PRIMARY KEY (`char_guid`,`guid`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-/*Data for the table `characters_social` */
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`mangosvbcharacters` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+/*Table structure for table `character_social` */
+
+DROP TABLE IF EXISTS `character_social`;
+
+CREATE TABLE `character_social` (
+  `guid` int(11) unsigned NOT NULL DEFAULT '0' COMMENT 'Character Global Unique Identifier',
+  `friend` int(11) unsigned NOT NULL DEFAULT '0' COMMENT 'Friend Global Unique Identifier',
+  `flags` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT 'Friend Flags',
+  PRIMARY KEY (`guid`,`friend`,`flags`),
+  KEY `guid` (`guid`),
+  KEY `friend` (`friend`),
+  KEY `guid_flags` (`guid`,`flags`),
+  KEY `friend_flags` (`friend`,`flags`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Player System';
+
+/*Data for the table `character_social` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
