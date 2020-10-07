b8f53c8d6feba220024ca329a3525228c7f234ed
Antz <billy1arm@users.noreply.github.com>
2020-10-07 13:40:03 +0100

    Implemented creature_template_spells

--- World/Setup/mangosdLoadDB.sql
+++ World/Setup/mangosdLoadDB.sql
@@ -368,6 +368,24 @@ CREATE TABLE `creature_template` (
 
 
 --
+-- Table structure for table `creature_template_spells`
+--
+
+DROP TABLE IF EXISTS `creature_template_spells`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `creature_template_spells` (
+  `entry` mediumint(8) unsigned NOT NULL COMMENT 'This references the unique ID in table creature_template.',
+  `spell1` mediumint(8) unsigned NOT NULL COMMENT 'The spell identifier. The value has to match with a defined spell identifier.',
+  `spell2` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT 'The spell identifier. The value has to match with a defined spell identifier.',
+  `spell3` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT 'The spell identifier. The value has to match with a defined spell identifier.',
+  `spell4` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT 'The spell identifier. The value has to match with a defined spell identifier.',
+  PRIMARY KEY (`entry`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Creature System (Spells used by creature)';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
 -- Table structure for table `disables`
 --
 
